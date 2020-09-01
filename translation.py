e8337042f58900102ddd5918125c4342c7ec077d
00Satyu00 <70587164+00Satyu00@users.noreply.github.com>
2020-09-01 21:36:17 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,10 +1,10 @@
 class Translation(object):
-    START_TEXT = """Hello,
-This is a Telegram Rename Bot!
-
-<b>Please send me any Telegram file and reply to that file to /rename New Name.mkv</b>
-
-/help for more details.."""
+    
+    
+    
+    
+    
+    
     RENAME_403_ERR = "Sorry. You are not permitted to rename this file."
     ABS_TEXT = " Please don't be selfish."
     UPGRADE_TEXT = "<b>Contact :- @shreevish"
